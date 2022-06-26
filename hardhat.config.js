6d2dbe76850c045549131197febec77e5ce1fcfa
Luis Fernando Ushiña <guffenix@gmail.com>
2022-06-26 13:31:32 -0500

    initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,8 @@
+require('@nomiclabs/hardhat-ethers')
+require('@openzeppelin/hardhat-upgrades')
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: '0.8.15',
+}
